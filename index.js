522368b691dfb3456a0a336fd18e70f1a1280053
BouchraDjalti <boucradjalti369@gmail.com>
2023-09-15 12:19:11 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
-}
+
+    static capitalize(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+  
+    static sanitize(str) {
+      return str.replace(/[^a-zA-Z0-9-' ]/g, '');
+    }
+  
+    static titleize(str) {
+      const smallWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+  
+      const words = str.split(' ');
+  
+      for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        if (i === 0 || !smallWords.includes(word.toLowerCase())) {
+          words[i] = Formatter.capitalize(word);
+        }
+      }
+  
+      return words.join(' ');
+    }
+  }
+  
+ 
